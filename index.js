a17c154a45d390dc377563164e5b95d50fd89489
aurbelis <aurbelis@shift4.com>
2018-05-10 14:44:14 +0300

    screenshot

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cluster = require('cluster');
-const app = require('express')();
+const express = require('express');
+
 const getScreenshot = require('./modules/screenshot');
 const numberOfWorkers = require('os').cpus().length;
 
@@ -7,8 +8,9 @@ if (cluster.isMaster) {
 	console.log('Master with %s workers', numberOfWorkers);
 
 	for (let i = 0; i < numberOfWorkers; i += 1) {
-		const worker = cluster.fork().process;
-		console.log('Worker %s started.', worker.pid);
+		cluster.fork();
+		// const worker = cluster.fork();
+		// console.log('Worker %s started.', worker.pid);
 	}
 
 	cluster.on('exit', (worker) => {
@@ -16,8 +18,18 @@ if (cluster.isMaster) {
 		cluster.fork();
 	});
 } else {
+
+	const app = express();
+
 	app.get('/screenshot', async (req, res) => {
 		try {
+
+			let sum = 0;
+
+			for (let i = 0; i <1000000; i+=1) {
+				sum+1;
+			}
+
 			console.log(`WorkerId: ${process.pid}`);
 
 			const url = req.query.url;
@@ -40,7 +52,7 @@ if (cluster.isMaster) {
 		}
 	});
 
-	app.listen(5001, () => console.log('Listening on port 5001'));
+	app.listen(5001, () => console.log(`ID ${process.pid} Listening on port 5001`));
 }
 
 process.on('uncaughtException', (err) => {
--- modules/screenshot.js
+++ modules/screenshot.js
@@ -1,13 +1,46 @@
 const puppeteer = require('puppeteer');
 
 module.exports = async (url) => {
-	const browser = await puppeteer.launch();
+	console.log('launch...');
+	const browser = await puppeteer.launch({
+		ignoreHTTPSErrors: true,
+		args: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-dev-shm-usage', '--start-maximized']
+	});
+
+	console.log('newPage...');
 	const page = await browser.newPage();
-	page.setViewport({ width: 1024, height: 768 });
-	await page.goto(url, { timeout: 18000 });
-	const result = await page.screenshot({ type: 'png', fullPage: true });
-	await page.waitFor(500);
+	await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36');
+
+	console.log('goto...');
+
+	await page.goto(url, {
+		waitUntil: 'load',
+		timeout: 60000
+	});
+
+	// const bodyHandle = await page.$('body');
+	// const { width, height } = await bodyHandle.boundingBox();
+	console.log('setViewport...');
+	await page.setViewport({ width: 1920, height: 1024 });
+
+	console.log('screenshot...');
+	const result = await page.screenshot({fullPage: true});
+	// const result = await page.screenshot({
+	// 	clip: {
+	// 		x: 0,
+	// 		y: 0,
+	// 		width,
+	// 		height
+	// 	},
+	// 	type: 'png'
+	// });
+
+
+	//await bodyHandle.dispose();
+
+	console.log('close...');
 	await browser.close();
 
+	console.log('return...');
 	return result;
 };
